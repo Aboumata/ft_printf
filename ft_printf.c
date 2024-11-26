97bccb5c552d798b70ad5e5f867c5197001298d1
aboumata <fanchkhow@gmail.com>
2024-11-26 19:03:55 +0100

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -12,75 +12,105 @@
 
 #include "ft_printf.h"
 
-static void	ft_putchar(char c, int *count)
+static int	ft_putchar(char c, int *count)
 {
-	*count += write(1, &c, 1);
+	int	written;
+
+	written = write(1, &c, 1);
+	if (written == -1)
+		return (-1);
+	*count += written;
+	return (0);
 }
 
-static void	ft_putstr(const char *str, int *count)
+static int	ft_putstr(const char *str, int *count)
 {
 	if (str == NULL)
 	{
-		*count += write(1, "(null)", 6);
-		return ;
+		if (write(1, "(null)", 6) == -1)
+			return (-1);
+		*count += 6;
+		return (0);
 	}
 	while (*str)
-		ft_putchar(*str++, count);
+	{
+		if (ft_putchar(*str++, count) == -1)
+			return (-1);
+	}
+	return (0);
 }
 
-static void	ft_putnbr_base(unsigned long long nbr, const char *base,
+static int	ft_putnbr_base(unsigned long long nbr, const char *base,
 		int base_len, int *count)
 {
 	if (nbr >= (unsigned long long)base_len)
-		ft_putnbr_base(nbr / base_len, base, base_len, count);
-	ft_putchar(base[nbr % base_len], count);
-}
-
-static void	ft_putnbr_signed(int nbr, int *count)
-{
-	if (nbr < 0)
 	{
-		ft_putchar('-', count);
-		ft_putnbr_base(-(unsigned long long)nbr, "0123456789", 10, count);
+		if (ft_putnbr_base(nbr / base_len, base, base_len, count) == -1)
+			return (-1);
 	}
-	else
-		ft_putnbr_base((unsigned long long)nbr, "0123456789", 10, count);
+	if (ft_putchar(base[nbr % base_len], count) == -1)
+		return (-1);
+	return (0);
 }
 
-static void	ft_putpointer(void *ptr, int *count)
+static int	ft_putpointer(void *ptr, int *count)
 {
 	uintptr_t	addr;
 
 	addr = (uintptr_t)ptr;
-	*count += write(1, "0x", 2);
+	if (write(1, "0x", 2) == -1)
+		return (-1);
+	*count += 2;
 	if (addr == 0)
 	{
-		*count += write(1, "0", 1);
-		return ;
+		if (write(1, "0", 1) == -1)
+			return (-1);
+		*count += 1;
+		return (0);
 	}
-	ft_putnbr_base(addr, "0123456789abcdef", 16, count);
+	return (ft_putnbr_base(addr, "0123456789abcdef", 16, count));
 }
 
-static void	ft_process_format(char spec, va_list args, int *count)
+static int	ft_putnbr_signed(int nbr, int *count)
+{
+	if (nbr == INT_MIN)
+	{
+		if (ft_putstr("-2147483648", count) == -1)
+			return (-1);
+		return (0);
+	}
+	if (nbr < 0)
+	{
+		if (ft_putchar('-', count) == -1)
+			return (-1);
+		nbr = -nbr;
+	}
+	return (ft_putnbr_base((unsigned long long)nbr, "0123456789", 10, count));
+}
+
+
+static int	ft_process_format(char spec, va_list args, int *count)
 {
 	if (spec == 'c')
-		ft_putchar(va_arg(args, int), count);
+		return (ft_putchar(va_arg(args, int), count));
 	else if (spec == 's')
-		ft_putstr(va_arg(args, const char *), count);
+		return (ft_putstr(va_arg(args, const char *), count));
 	else if (spec == 'p')
-		ft_putpointer(va_arg(args, void *), count);
+		return (ft_putpointer(va_arg(args, void *), count));
 	else if (spec == 'd' || spec == 'i')
-		ft_putnbr_signed(va_arg(args, int), count);
+		return (ft_putnbr_signed(va_arg(args, int), count));
 	else if (spec == 'u')
-		ft_putnbr_base(va_arg(args, unsigned int), "0123456789", 10, count);
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789", 10,
+				count));
 	else if (spec == 'x')
-		ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef", 16,
-			count);
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef",
+				16, count));
 	else if (spec == 'X')
-		ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF", 16,
-			count);
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF",
+				16, count));
 	else if (spec == '%')
-		ft_putchar('%', count);
+		return (ft_putchar('%', count));
+	return (0);
 }
 
 int	ft_printf(const char *format, ...)
@@ -93,9 +123,18 @@ int	ft_printf(const char *format, ...)
 	while (*format)
 	{
 		if (*format == '%' && *(format + 1))
-			ft_process_format(*(++format), args, &count);
-		else
-			ft_putchar(*format, &count);
+		{
+			if (ft_process_format(*(++format), args, &count) == -1)
+			{
+				va_end(args);
+				return (-1);
+			}
+		}
+		else if (ft_putchar(*format, &count) == -1)
+		{
+			va_end(args);
+			return (-1);
+		}
 		format++;
 	}
 	va_end(args);
@@ -106,7 +145,10 @@ int	main(void)
 {
 	ft_printf("Character: %c\n", 'A');
 	ft_printf("String: %s\n", "Hello, world!");
-	ft_printf("Pointer: %p\n", (void *)0x7ffeefbff5c8);
+	ft_printf("%p\n", NULL);
+	ft_printf("%p %p\n", (void *)LONG_MIN, (void *)LONG_MAX);
+	ft_printf("%p\n", (void *)ULONG_MAX);
+	ft_printf("%p\n", (void *)(-ULONG_MAX));
 	ft_printf("Decimal: %d\n", -42);
 	ft_printf("Integer: %i\n", 123);
 	ft_printf("Unsigned: %u\n", 3000000000U);
